c94237e4b9a3a397915de060526ece5cc5f5d347
joseph <joseph@pc>
2024-10-13 13:50:06 +1100

    Add keybinds

--- init.lua
+++ init.lua
@@ -201,6 +201,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- My own keybinds?
+vim.keymap.set('n', '<leader>md', vim.diagnostic.open_float, { desc = 'Show diagnostic in float' })
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
